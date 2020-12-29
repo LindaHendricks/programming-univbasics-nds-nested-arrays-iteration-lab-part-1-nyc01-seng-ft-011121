54fa564b5737aa20d959416e924fae4b98babac2
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-29 21:01:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,9 @@ count = 0
  while count < nested_array_1[count].length do 
   inner_count = 0 
    while inner_count < nested_array_1[count].length do 
-      if inner_count == 2%  
+        inner_count == 2%  
         nested_array_1[count][inner_count]
         inner_count += 1 
-      end 
     end 
     count +=1
   end 
