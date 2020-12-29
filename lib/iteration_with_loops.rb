ab0e1ec93a7c99992e318e1c97df1c1260f88650
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-29 20:57:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,13 +6,13 @@ def find_even_values(nested_array_1)
 count = 0 
  while count < nested_array_1[count].length do 
   inner_count = 0 
-   while inner_count < nested_array_1[count][inner_count].length do 
+   while inner_count < nested_array_1[count].length do 
       if inner_count == 2%  
-       puts nested_array_1[count][inner_count]
+        nested_array_1[count][inner_count]
         inner_count += 1 
       end 
-    count +=1 
     end 
+    count +=1
   end 
 end 
  
