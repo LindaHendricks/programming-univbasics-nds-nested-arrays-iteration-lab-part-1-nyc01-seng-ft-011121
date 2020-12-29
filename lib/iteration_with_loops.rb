d207d9c7faf22d79f537e9f346fe0209abf81669
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-29 21:45:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,20 @@
 
-nested_array_1 = [ [10, 11], [99, 50, 3, 4], [23, 41] 
+
 
 
 def find_even_values (nested_array_1)
   
+  nested_array_1 = [ [10, 11], [99, 50, 3, 4], [23, 41] 
   row_index = 0
   
 while row_index < nested_array_1.count do
   element_index = 0
+ 
   while element_index < nested_array_1[row_index].count do
     p nested_array_1[row_index][element_index]
     element_index += 1
   end
+ 
   row_index += 1
 end 
 
